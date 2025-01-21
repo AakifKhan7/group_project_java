e432c4968d061c143adf3a920a32e5f09504886a
Aakifkhan7 <baloochaakifkhan@gmail.com>
2025-01-21 23:50:56 +0530

    main option added for client and customer

--- main.java
+++ main.java
@@ -1,5 +1,25 @@
+import java.util.*;
+
 class restaurant {
     public static void main(String[] args) {
-        System.out.println("Welcome to the restaurant");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1. Manager");
+        System.out.println("2. Customer");
+        System.out.println("3. Exit");
+        System.out.print("Enter your choice: ");
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.println("Manager");
+                break;
+            case 2:
+                System.out.println("Customer");
+                break;
+            case 3:
+                System.out.println("Exit");
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }
     }
 }
