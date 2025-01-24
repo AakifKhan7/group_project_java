b9e327b1f4850bc331348960e494b20fcb3a7d3e
Neel Patel <neelchirag235@gmail.com>
2025-01-24 18:51:41 +0530

    Update main.java
    added menu

--- main.java
+++ main.java
@@ -29,5 +29,29 @@ class Customer{
 }
 class Manager{
     System.out.println("Choose your Food from given menue");
-    
+    System.out.println("Choose number for given type of food\n\t1.vegetarian\n2.Non-Vegetarian ");
+    int choiceFood = sc.nextInt();
+    if(choiceFood==1){
+        System.out.println("You chose Vegetatian food:-");
+        System.out.println("In Vegtarian we have more than 100 dishes,kindly chose your favourite from given list......");
+        Sytem.out.println("1. Indian Vegetarian Dishes:\n" + //
+                        "Starters & Snacks:\n" + //
+                        "\n" + //
+                        "Paneer Tikka\n" + //
+                        "Samosa\n" + //
+                        "Aloo Tikki\n" + //
+                        "Pakoras (Vegetable, Paneer, Onion)\n" + //
+                        "Dhokla\n" + //
+                        "Bhel Puri\n" + //
+                        "Main Course:\n" + //
+                        "\n" + //
+                        "Paneer Butter Masala\n" + //
+                        "Chole (Chickpea Curry)\n" + //
+                        "Dal Makhani\n" + //
+                        "Baingan Bharta (Mashed Eggplant)\n" + //
+                        "Palak Paneer (Spinach and Cottage Cheese)\n" + //
+                        "Veg Biryani\n" + //
+                        "Rajma (Kidney Bean Curry)")
+    }
+
 }
