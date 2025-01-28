53537429eba30fded7b1db7c4bca4f0bacbcb54c
Aakifkhan7 <baloochaakifkhan@gmail.com>
2025-01-29 00:34:39 +0530

    added bill option

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class restaurant {
+class Restaurant {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("1. Manager");
@@ -8,6 +8,7 @@ class restaurant {
         System.out.println("3. Exit");
         System.out.print("Enter your choice: ");
         int choice = sc.nextInt();
+
         switch (choice) {
             case 1:
                 System.out.println("Manager");
@@ -15,7 +16,7 @@ class restaurant {
             case 2:
                 Customer customer = new Customer();
                 customer.selectFood();
-            break;
+                break;
             case 3:
                 System.out.println("Exit");
                 break;
@@ -30,34 +31,45 @@ class Customer {
     Scanner sc = new Scanner(System.in);
     Menu menu = new Menu();
 
+
     Customer() {
         System.out.println("Welcome to virtual restaurant");
     }
 
-    
+    void selectFood() {
 
-    void selectFood(){
 
-        System.out.println("Choose your Food from given menu");
         System.out.println("Choose number for given type of food\n\t1.vegetarian\n2.Non-Vegetarian ");
         int choiceFood = sc.nextInt();
 
-
-        if(choiceFood==1){
+        System.out.println("Choose your Food from given menu");
+        if (choiceFood == 1) {
             menu.vegetarianFood();
-        }
-        else if(choiceFood==2){
+        } else if (choiceFood == 2) {
             menu.nonVegetarian();
-        }
-        else{
+        } else {
             System.out.println("Invalid choice");
+            return;
+        }
+        
+        
+        System.out.println("How many items you want to order?");
+        int numberOfItems = sc.nextInt();
+        int[] selectedFood = new int[numberOfItems];
+        for (int itemCount = 0; itemCount < numberOfItems; itemCount++) {
+            System.out.print("Enter the number of your dish (1 to " + menu.vegMenu.length + "): ");
+            int selectedDish = sc.nextInt();
+            if (selectedDish >= 1 && selectedDish <= menu.vegMenu.length) {
+                selectedFood[itemCount] = selectedDish;
+            } else {
+                System.out.println("Invalid dish number, please try again.");
+                itemCount--;  // Decrement to retry for this item
+            }
         }
 
-
-        System.out.print("Enter Given number of your dishes:");
-        int selectedDish=sc.nextInt();
+        Bill bill = new Bill();
+        bill.calculateBill(selectedFood);
     }
-
 
     void payment() {
         System.out.println("Choose your payment method");
@@ -86,7 +98,6 @@ class Customer {
                 System.out.println("Invalid choice");
         }
 
-
     }
 
     void feedback() {
@@ -94,46 +105,70 @@ class Customer {
     }
 }
 
-class Menu{
-    String[] vegMenu;
-    String[] nonVegMenu;
+class Menu {
+    
+    int sizeOfVegPrice = 20;
+    String[] vegMenu = new String[sizeOfVegPrice];
+    int[] vegPrice = new int[sizeOfVegPrice];
+
+    int sizeOfNonVegPrice = 20;
+    String[] nonVegMenu = new String[sizeOfNonVegPrice];
+    int[] nonVegPrice = new int[sizeOfNonVegPrice];
+
     void vegetarianFood() {
         System.out.println("You chose Vegetarian food:-");
-        System.out.println("In Vegetarian we have more than 100 dishes,kindly chose your favourite from given list......");
+        System.out.println(
+                "In Vegetarian we have more than 100 dishes,kindly chose your favourite from given list......");
         System.out.println("Here’s a list of popular vegetable-based dishes");
-        String menu = "### **Indian Vegetable Dishes**\n" +
-                "1. Paneer Butter Masala\n" +
-                "2. Aloo Gobi (Potato & Cauliflower Curry)\n" +
-                "3. Baingan Bharta (Roasted Eggplant Curry)\n" +
-                "4. Palak Paneer (Spinach with Cottage Cheese)\n" +
-                "5. Chana Masala (Chickpea Curry)\n" +
-                "6. Veg Biryani\n" +
-                "7. Dum Aloo (Potato Curry)\n" +
-                "8. Bhindi Masala (Okra Curry)\n" +
-                "9. Mixed Vegetable Curry\n" +
-                "10. Matar Paneer (Peas and Cottage Cheese Curry)\n\n" +
-                "### **International Vegetable Dishes**\n" +
-                "11. Ratatouille (French Vegetable Stew)\n" +
-                "12. Veggie Stir-Fry (Asian Style)\n" +
-                "13. Stuffed Bell Peppers (Mediterranean Style)\n" +
-                "14. Grilled Vegetable Skewers\n" +
-                "15. Vegetable Lasagna\n" +
-                "16. Thai Green Curry with Vegetables\n" +
-                "17. Cream of Mushroom Soup\n" +
-                "18. Veggie Tacos (Mexican Style)\n" +
-                "19. Veggie Sushi Rolls\n" +
-                "20. Greek Salad\n\n" +
-                "### **Snacks and Starters**\n";
-                vegMenu = menu.split("\n");
+        String menu = "1. Paneer Butter Masala - 250\n" +
+                "2. Aloo Gobi (Potato & Cauliflower Curry) - 200\n" +
+                "3. Baingan Bharta (Roasted Eggplant Curry) - 220\n" +
+                "4. Palak Paneer (Spinach with Cottage Cheese) - 240\n" +
+                "5. Chana Masala (Chickpea Curry) - 210\n" +
+                "6. Veg Biryani - 300\n" +
+                "7. Dum Aloo (Potato Curry) - 230\n" +
+                "8. Bhindi Masala (Okra Curry) - 200\n" +
+                "9. Mixed Vegetable Curry - 250\n" +
+                "10. Matar Paneer (Peas and Cottage Cheese Curry) - 260\n\n" +
+                // "### **International Vegetable Dishes**\n" +
+                "11. Ratatouille (French Vegetable Stew) - 280\n" +
+                "12. Veggie Stir-Fry (Asian Style) - 320\n" +
+                "13. Stuffed Bell Peppers (Mediterranean Style) - 300\n" +
+                "14. Grilled Vegetable Skewers - 270\n" +
+                "15. Vegetable Lasagna - 350\n" +
+                "16. Thai Green Curry with Vegetables - 290\n" +
+                "17. Cream of Mushroom Soup - 180\n" +
+                "18. Veggie Tacos (Mexican Style) - 250\n" +
+                "19. Veggie Sushi Rolls - 220\n" +
+                "20. Greek Salad - 200\n\n";
+        // "### **Snacks and Starters**\n";
 
-                for(String item : vegMenu){
-                    System.out.println(item);
+        vegMenu = menu.split("\n");
+
+        String[] vegMenuItems = menu.split("\n");
+
+        int index = 0;
+        for (String item : vegMenuItems) {
+            if (item.contains("-")) {
+                // Split by '-' to separate the item name and the price
+                String[] parts = item.split(" - ");
+                if (parts.length == 2) {
+                    vegMenu[index] = parts[0].trim(); // Menu item name
+                    vegPrice[index] = Integer.parseInt(parts[1].trim()); // Menu item price
+                    index++;
                 }
+            }
+        }
+
+        for (int i = 0; i < vegMenu.length - 1; i++) {
+            System.out.println(vegMenu[i] + " " + vegPrice[i] + "INR");
+        }
     }
 
-    void nonVegetarian(){
+    void nonVegetarian() {
         System.out.println("You chose Non-Vegetarian food:-");
-        System.out.println( "In Non-Vegetarian we have more than 100 dishes,kindly chose your favourite from given list......");
+        System.out.println(
+                "In Non-Vegetarian we have more than 100 dishes,kindly chose your favourite from given list......");
         System.out.println("Here’s a list of popular Non-vegetable-based dishes");
         String menu = "### **Indian Non-Vegetarian Dishes**\n" +
                 "1. Butter Chicken\n" +
@@ -159,26 +194,46 @@ class Menu{
                 "20. Beef Wellington\n\n" +
                 "### **Snacks and Starters**\n";
 
-                nonVegMenu = menu.split("\n");
+        nonVegMenu = menu.split("\n");
 
-                // print menu
-                for (String item : nonVegMenu) {
-                    System.out.println(item);
-                }
+        // print menu
+        for (String item : nonVegMenu) {
+            System.out.println(item);
+        }
     }
 }
 
+class Bill{
+    Menu menu = new Menu();
+    String[] item = menu.vegMenu;
+    int[] price = menu.vegPrice;
+    void calculateBill(int[] selectedFood) {
+        int billTotal = 0;
+        for (int i = 0; i < selectedFood.length; i++) {
+            int dishIndex = selectedFood[i] - 1;
+            if (dishIndex >= 0 && dishIndex < item.length) {
+                System.out.println(item[dishIndex] + " - Rs." + price[dishIndex]);
+                billTotal += price[dishIndex];
+            } else {
+                System.out.println("Invalid dish selected.");
+            }
 
-class Payment{
+        }
+        System.out.println("Your total bill is: " + billTotal);
+    }
+}
+
+class Payment {
     Scanner sc = new Scanner(System.in);
-    void payCash(int billTotal){
+
+    void payCash(int billTotal) {
         double amount = 0;
-        do{
+        do {
             System.out.println("Enter amount");
             amount = sc.nextDouble();
-        }while (amount < billTotal);
-            
-        if(amount >= billTotal){
+        } while (amount < billTotal);
+
+        if (amount >= billTotal) {
             System.out.println(amount - billTotal + "You get return");
         }
 
